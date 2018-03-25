a21041c9da05653ff279c3880c02c765bca68c86
Luthfi <vlootfie@gmail.com>
2018-03-26 02:02:19 +0700

    fixing server

--- server/routes/login.js
+++
@@ -1,6 +0,0 @@
-var express = require('express');
-var router = express.Router();
-const usersController = require('../controllers/usersController')
-/* GET users listing. */
-router.post('/', usersController.login)
-module.exports = router;
